4b1aec8d59d8bf48a7ca78ad371a6dc6c4758ec4
Vít Kotačka <vit.kotacka@gmail.com>
2017-03-18 09:35:22 +0100

    Remaining functions and tests have been added.

--- src/blog_catalan/core.clj
+++ src/blog_catalan/core.clj
@@ -3,3 +3,23 @@
 (defn factorial [n]
   "Counts a factorial."
   (reduce * (range 1 (inc n))))
+
+(defn binomial [n k]
+  "Counts a binomial coefficient."
+  (/ (factorial n)
+     (* (factorial k)
+        (factorial (- n k)))))
+
+(defn catalan [n]
+  "Counts a Catalan number."
+  (* (/ 1 (inc n))
+     (binomial (* 2 n) n)))
+
+(defn catalans
+  "Returns a lazy seq of Catalan numbers."
+  ([]
+    (map catalan (range)))
+  ([end]
+    (map catalan (range end)))
+  ([start end]
+    (map catalan (range start end))))
--- test/blog_catalan/core_test.clj
+++ test/blog_catalan/core_test.clj
@@ -16,3 +16,34 @@
   7    5040
   8   40320
   9  362880)
+
+(tabular
+  (fact "The binomial"
+        (binomial ?n ?k) => ?b)
+  ?n ?k ?b
+  0  0  1
+  1  0  1
+  2  0  1
+  2  1  2
+  3  2  3
+  4  2  6
+  6  3  20)
+
+(tabular
+  (fact "The Catalan numbers"
+        (catalan ?n) => ?c)
+  ?n ?c
+  0  1
+  1  1
+  2  2
+  3  5
+  4  14)
+
+(fact "Seq of the 1st ten Catalan numbers"
+      (take 10 (catalans)) => (contains [1 1 2 5 14 42 132 429 1430 4862]))
+
+(fact "Seq of the Catalan numbers up to 10"
+      (catalans 10) => (contains [1 1 2 5 14 42 132 429 1430 4862]))
+
+(fact "Seq of fthe Catalan numbers from 5 to 10"
+      (catalans 5 10) => (contains [42 132 429 1430 4862]))
