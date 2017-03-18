e54a6ff9fa7a64ff1d6a86eac654e5df42edd219
Vít Kotačka <vit.kotacka@gmail.com>
2017-03-18 11:22:03 +0100

    Tabular tests has been extended.

--- test/blog_catalan/core_test.clj
+++ test/blog_catalan/core_test.clj
@@ -20,24 +20,50 @@
 (tabular
   (fact "The binomial"
         (binomial ?n ?k) => ?b)
-  ?n ?k ?b
-  0  0  1
-  1  0  1
-  2  0  1
-  2  1  2
-  3  2  3
-  4  2  6
-  6  3  20)
+  ?n  ?k  ?b
+  0   0   1
+  1   0   1
+  1   1   1
+  2   0   1
+  2   1   2
+  2   2   1
+  3   0   1
+  3   1   3
+  3   2   3
+  3   3   1
+  4   0   1
+  4   1   4
+  4   2   6
+  4   3   4
+  4   4   1
+  5   0   1
+  5   1   5
+  5   2  10
+  5   3  10
+  5   4   5
+  5   5   1
+  6   0   1
+  6   1   6
+  6   2  15
+  6   3  20
+  6   4  15
+  6   5   6
+  6   6   1)
 
 (tabular
   (fact "The Catalan numbers"
         (catalan ?n) => ?c)
-  ?n ?c
-  0  1
-  1  1
-  2  2
-  3  5
-  4  14)
+  ?n    ?c
+  0     1
+  1     1
+  2     2
+  3     5
+  4    14
+  5    42
+  6   132
+  7   429
+  8  1430
+  9  4862)
 
 (fact "Seq of the 1st ten Catalan numbers"
       (take 10 (catalans)) => (contains [1 1 2 5 14 42 132 429 1430 4862]))
